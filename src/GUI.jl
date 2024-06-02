ebe0df49ba0705fe4cb52c7cfa4afb06c2d95f38
Harindu Bandara <harindudb@gmail.com>
2024-06-02 21:04:45 +0530

    revamp, seperate gui and interface connected by mqtt

--- src/GUI.jl
+++ src/GUI.jl
@@ -1,13 +1,39 @@
 module GUI
 using Mousetrap
+using Mosquitto
 import Reactive as Rx
 import JSON
 
-appsignal = Rx.Signal("")
-foreach(x -> println(x), appsignal)
+client = Client("localhost", 1883) 
+topic = Dict(
+    "mix" => "dexioscs/value/mix",
+    "mmd" => "dexioscs/value/mmd",
+    "mrl" => "dexioscs/value/mrl",
+    "mtf" => "dexioscs/value/mtf",
+    "mto" => "dexioscs/value/mto",
+    "six" => "dexioscs/value/six",
+    "smd" => "dexioscs/value/smd",
+    "mode" => "dexioscs/mode",
+    "grasp" => "dexioscs/grasp",
+)
+
+function pubmsg(topic,message)
+    publish(client, topic, message)
+    loop(client)
+end
 
 const scaleSize = 300
-scaler = 120
+
+function setspeed(value)
+    return floor(Int, 5.1 * (value - 50)) |> string
+end
+
+function setsetpoint(value)
+    return floor(Int, 120 * (value)) |> string
+end
+
+scalerfunc = setsetpoint
+
 
 main() do app::Application
     set_current_theme!(app, THEME_DEFAULT_DARK)
@@ -15,61 +41,182 @@ main() do app::Application
     paned = Paned(ORIENTATION_HORIZONTAL)
     grid = Grid()
     box = Box(ORIENTATION_VERTICAL)
+
     dropdown = DropDown()
+    set_margin!(dropdown, 10)
+    graspselect = DropDown()
+    set_margin!(graspselect, 10)
+    set_is_visible!(graspselect, false)
 
     set_start_child!(paned, box)
     set_end_child!(paned, grid)
 
     push_back!(box, dropdown)
+    push_back!(box, graspselect)
 
-    push_back!(dropdown, "PID") do self::DropDown
-        global scaler = 120
+    push_back!(dropdown, "Reset") do self::DropDown
+        set_is_visible!(mix, true)
+        set_is_visible!(mmd, true)
+        set_is_visible!(mrl, true)
+        set_is_visible!(mtf, true)
+        set_is_visible!(mto, true)
+        set_is_visible!(six, true)
+        set_is_visible!(smd, true)
+        set_is_visible!(graspselect, false)
         return nothing
-        
+    end
+    push_back!(dropdown, "PID") do self::DropDown
+        global scalerfunc = setsetpoint
+        set_value!(mix, 0)
+        set_value!(mmd, 0)
+        set_value!(mrl, 0)
+        set_value!(mtf, 0)
+        set_value!(mto, 0)
+        pubmsg(topic["mode"], "pid")
+        return nothing
     end
     push_back!(dropdown, "Manual") do self::DropDown
-        global scaler = 2.55
+        global scalerfunc = setspeed
+        set_value!(mix, 50)
+        set_value!(mmd, 50)
+        set_value!(mrl, 50)
+        set_value!(mtf, 50)
+        set_value!(mto, 50)
+        pubmsg(topic["mode"], "manual")
+        return nothing
+    end
+    push_back!(dropdown, "Grasp") do self::DropDown
+        set_is_visible!(mix, false)
+        set_is_visible!(mmd, false)
+        set_is_visible!(mrl, false)
+        set_is_visible!(mtf, false)
+        set_is_visible!(mto, false)
+        set_is_visible!(six, false)
+        set_is_visible!(smd, false)
+        set_is_visible!(graspselect, true)
+        pubmsg(topic["mode"], "grasp")
+        return nothing
+    end
+
+    push_back!(graspselect, "None") do self::DropDown
+        return nothing
+    end
+    push_back!(graspselect, "Pinch") do self::DropDown
+        #dict = Dict("action" => "graspsel", "data" => "pinch")
+        #put!(channel, JSON.json(dict))
+        pubmsg(topic["grasp"], "pinch")
+        return nothing
+    end
+    push_back!(graspselect, "Spherical") do self::DropDown
+        #dict = Dict("action" => "graspsel", "data" => "spherical")
+        #put!(channel, JSON.json(dict))
+        pubmsg(topic["grasp"], "spherical")
         return nothing
     end
 
     mix = Scale(0, 100, 1)
-    set_size_request!(mix,Vector2f(scaleSize, 0))
+    set_margin!(mix, 10)
+    set_expand_horizontally!(mix, true)
+    set_size_request!(mix, Vector2f(scaleSize, 0))
     mix_label = Label("Motor Index")
     connect_signal_value_changed!(mix) do self::Scale
-        value = floor(Int,scaler*get_value(self))
-        dict = Dict("action"=>"setval","data" => Dict("mix"=>value))
-        push!(appsignal, JSON.json(dict))
+        value = scalerfunc(get_value(self))
+        #dict = Dict("action" => commandString, "data" => Dict("mix" => value))
+        #put!(channel, JSON.json(dict))
+        pubmsg(topic["mix"], value)
+        return nothing
+    end
+    mix_reset = # TODO add button to reset to zero level
+        mmd = Scale(0, 100, 1)
+    set_margin!(mmd, 10)
+    set_size_request!(mmd, Vector2f(scaleSize, 0))
+    mmd_label = Label("Motor Middle")
+    connect_signal_value_changed!(mmd) do self::Scale
+        value = scalerfunc(get_value(self))
+        #dict = Dict("action" => commandString, "data" => Dict("mmd" => value))
+        #put!(channel, JSON.json(dict))
+        pubmsg(topic["mmd"], value)
         return nothing
     end
 
-    mmd = Scale(0, 100, 1)
-    set_size_request!(mmd,Vector2f(scaleSize, 0))
-    mmd_label = Label("Motor Middle")
-    connect_signal_value_changed!(mmd) do self::Scale
-        value = floor(Int,scaler*get_value(self))
-        dict = Dict("action"=>"setval","data" => Dict("mmd"=>value))
-        push!(appsignal, JSON.json(dict))
+    mrl = Scale(0, 100, 1)
+    set_margin!(mrl, 10)
+    set_size_request!(mrl, Vector2f(scaleSize, 0))
+    mrl_label = Label("Motor Ring/Little")
+    connect_signal_value_changed!(mrl) do self::Scale
+        value = scalerfunc(get_value(self))
+        #dict = Dict("action" => commandString, "data" => Dict("mrl" => value))
+        #put!(channel, JSON.json(dict))
+        pubmsg(topic["mrl"], value)
+        return nothing
+    end
+
+    mtf = Scale(0, 100, 1)
+    set_margin!(mtf, 10)
+    set_size_request!(mtf, Vector2f(scaleSize, 0))
+    mtf_label = Label("Motor Thumb Flex")
+    connect_signal_value_changed!(mtf) do self::Scale
+        value = scalerfunc(get_value(self))
+        #dict = Dict("action" => commandString, "data" => Dict("mtf" => value))
+        #put!(channel, JSON.json(dict))
+        pubmsg(topic["mtf"], value)
+        return nothing
+    end
+
+    mto = Scale(0, 100, 1)
+    set_margin!(mto, 10)
+    set_size_request!(mto, Vector2f(scaleSize, 0))
+    mto_label = Label("Motor Thumb Opp")
+    connect_signal_value_changed!(mto) do self::Scale
+        value = scalerfunc(get_value(self))
+        #dict = Dict("action" => commandString, "data" => Dict("mto" => value))
+        #put!(channel, JSON.json(dict))
+        pubmsg(topic["mto"], value)
         return nothing
     end
 
     six = Switch()
+    set_margin!(six, 10)
     six_label = Label("Solenoid Index")
     set_alignment!(six, ALIGNMENT_CENTER)
     connect_signal_switched!(six) do self::Switch
-        println("Six: $( get_is_active(self)) ")
+        value = get_is_active(self) |> string
+        #dict = Dict("action" => "setval", "data" => Dict("six" => value)) # TODO setval nolonger available
+        #put!(channel, JSON.json(dict))
+        pubmsg(topic["six"], value)
+        return nothing
+    end
+
+    smd = Switch()
+    set_margin!(smd, 10)
+    smd_label = Label("Solenoid Index")
+    set_alignment!(smd, ALIGNMENT_CENTER)
+    connect_signal_switched!(smd) do self::Switch
+        value = get_is_active(self) |> string
+        #dict = Dict("action" => "setval", "data" => Dict("smd" => value))
+        #put!(channel, JSON.json(dict))
+        pubmsg(topic["smd"], value)
+        return nothing
     end
 
     insert_at!(grid, mix_label, 1, 1, 1, 1)
     insert_at!(grid, mix, 2, 1, 20, 1)
     insert_at!(grid, mmd_label, 1, 2, 1, 1)
     insert_at!(grid, mmd, 2, 2, 20, 1)
-    
+    insert_at!(grid, mrl_label, 1, 3, 1, 1)
+    insert_at!(grid, mrl, 2, 3, 20, 1)
+    insert_at!(grid, mtf_label, 1, 4, 1, 1)
+    insert_at!(grid, mtf, 2, 4, 20, 1)
+    insert_at!(grid, mto_label, 1, 5, 1, 1)
+    insert_at!(grid, mto, 2, 5, 20, 1)
     insert_at!(grid, six_label, 1, 6, 1, 1)
     insert_at!(grid, six, 2, 6, 1, 1)
+    insert_at!(grid, smd_label, 1, 7, 1, 1)
+    insert_at!(grid, smd, 2, 7, 1, 1)
 
     set_margin!(grid, 10)
     set_child!(window, paned)
     present!(window)
 end
-
+disconnect(client)
 end
--- src/Interface.jl
+++ src/Interface.jl
@@ -1,19 +1,122 @@
 module Interface
+using LibSerialPort
+using Reactive
+using JSON
+using Mosquitto
+using Match
 
-#include("Webapp.jl")
-include("Console.jl")
+topic = Dict(
+    "mix" => "dexioscs/value/mix",
+    "mmd" => "dexioscs/value/mmd",
+    "mrl" => "dexioscs/value/mrl",
+    "mtf" => "dexioscs/value/mtf",
+    "mto" => "dexioscs/value/mto",
+    "six" => "dexioscs/value/six",
+    "smd" => "dexioscs/value/smd",
+    "mode" => "dexioscs/mode",
+    "grasp" => "dexioscs/grasp",
+    "exit" => "dexioscs/exit",
+)
 
-#import .Webapp
-import .Console
+serialread = Signal("")
+serialwrite = Signal("")
+#serialjson = map(parseJSON, serialread, typ=Dict)
+#serialall = foldp(push!, [], serialjson)
 
-import Reactive
-import JSON
-import GenieFramework
+exit = true
 
+function serial_loop(sp::SerialPort, instream::Signal{String}, outstream::Signal{String})
+    user_input = ""
+    mcu_message = ""
+
+    println("Starting I/O loop. Press ESC [return] to quit")
+
+    foreach(x->write(sp, x), outstream)
+
+    while true
+        # Poll for new data without blocking
+        #@async user_input = readline(keep=true)
+        @async mcu_message *= String(nonblocking_read(sp))
+
+        occursin("\e", user_input) && exit()  # escape
+
+        # Send user input to device with ENTER
+        #if endswith(user_input, '\n')
+        #    write(sp, "$user_input")
+        #    user_input = ""
+        #end
+
+        
+        #foreach(x->println(x), outstream)
+
+        # Print response from device as a line
+        if occursin("\n", mcu_message)
+            lines = split(mcu_message, "\n")
+            while length(lines) > 1
+                #println(popfirst!(lines))
+                push!(instream, popfirst!(lines))
+            end
+            mcu_message = lines[1]
+        end
+
+        # Give the queued tasks a chance to run
+        sleep(0.001)
+    end
+end
+
+function console(args...)
+
+    if length(args) < 2
+        println("Usage: $(basename(@__FILE__)) port baudrate")
+        println("Available ports:")
+        list_ports()
+        return
+    end
+
+    # Open a serial connection to the microcontroller
+    mcu = open(args[1], parse(Int, args[2]))
+
+    serial_loop(mcu, args[3], args[4])
+end
+
+function onconnect(client)
+    # Check if something happened, else return 0
+    nmessages = Base.n_avail(get_connect_channel(client))
+    nmessages == 0 && return 0
+
+    # At this point, a connection or disconnection happened
+    for _ = 1:nmessages
+        conncb = take!(get_connect_channel(client))
+        if conncb.val == 1
+            println("Connection of client $(client.id) successfull (return code $(conncb.returncode))")
+            map(t->subscribe(client, t), values(topic))
+        elseif conncb.val == 0
+            println("Client $(client.id) disconnected")
+        end
+    end
+    return nmessages
+end
+
+function onmessage(client)
+    # Check if something happened, else return 0
+    nmessages = Base.n_avail(get_messages_channel(client))
+    nmessages == 0 && return 0
+
+    # At this point, a message was received, lets process it
+    for i = 1:nmessages
+        temp = take!(get_messages_channel(client))
+        if (temp.topic == topic["exit"] && String(temp.payload) == "yes")
+            global exit = false
+        end
+        
+        handlemsg(temp)
+    end
+    return nmessages
+end
 
 function parseJSON(jsonString::String)
     try
-        o = JSON.parse(jsonString)
+        o = parse(jsonString)
         return o
     catch e
         println(e)
@@ -21,20 +124,66 @@ function parseJSON(jsonString::String)
     return Dict()
 end
 
-#command = Reactive.Signal("")
-serialread = Reactive.Signal("")
-serialwrite = Reactive.Signal("")
-serialjson = Reactive.map(parseJSON, serialread, typ=Dict)
-serialall = Reactive.foldp(push!, [], serialjson)
-
-
-function run()
-    GenieFramework.Genie.loadapp()
-    GenieFramework.Server.isrunning() || GenieFramework.up(async=true)
-    @async Console.console("COM7", "115200", serialread, serialwrite)
-    foreach(x -> Reactive.push!(serialwrite, x), Main.App.appsignal)
+function actstring(actuator, value)
+    dict = Dict("action" => "setval", "val" => value, "actuator" => actuator)
+    return json(dict)
 end
 
-export serialread, serialwrite, serialjson, serialall
-
+function modestring(value)
+    dict = Dict("action" => "modesel", "val" => value)
+    return json(dict)
 end
+
+function graspstring(value)
+    dict = Dict("action" => "graspsel", "val" => value)
+    return json(dict)
+end
+
+function nothingstring()
+    dict = Dict("action" => "none")
+    return json(dict)
+end
+
+function handlemsg(msg)
+    str = @match msg.topic begin
+        "dexioscs/value/mix" => actstring("mix", parse(Int64, String(msg.payload)))
+        "dexioscs/value/mmd" => actstring("mmd", parse(Int64, String(msg.payload)))
+        "dexioscs/value/mrl" => actstring("mrl", parse(Int64, String(msg.payload)))
+        "dexioscs/value/mtf" => actstring("mtf", parse(Int64, String(msg.payload)))
+        "dexioscs/value/mto"=> actstring("mto", parse(Int64, String(msg.payload)))
+        "dexioscs/value/six" => actstring("six", parse(Bool, String(msg.payload)))
+        "dexioscs/value/smd" => actstring("smd", parse(Bool, String(msg.payload)))
+        "dexioscs/mode" => modestring(String(msg.payload))
+        "dexioscs/grasp" => graspstring(String(msg.payload))
+        _ => nothingstring()
+    end
+    #push!(serialwrite, str)
+    push!(serialwrite, str)
+end
+
+function main()
+    # Connect to a broker
+    client = Client("localhost", 1883)   
+    @async console("COM13", "115200", serialread, serialwrite)
+    # Messages will be put as a tuple in
+    # the channel Mosquitto.messages_channel.
+    
+
+    @async while exit
+        loop(client) # network loop
+        onconnect(client) # check for connection/disconnection
+        onmessage(client) # check for messages
+        sleep(0.001)
+    end
+
+    # Close everything
+    #disconnect(client)
+    #loop(client)
+    foreach(x -> println(x), serialread)
+end
+
+export main
+end
+
+Interface.main()
+# TODO write new loop incorporating serial, ditch Reactive
